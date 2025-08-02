a551890bf096d41eb3db918ec2669cd0f743a086
Gurjit Singh <gurjit.mail@gmail.com>
2025-08-01 19:32:08 -0700

    Setup tfstate remote backend in aws s3.

---
+++ aws/vpc/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "gsingh-terraform-state-bucket"
+    key            = "vpc/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+  }
+}
