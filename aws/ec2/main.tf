eae6fffaeb42b520090bb54a2e28e99f735c3899
Gurjit Singh <gurjit.mail@gmail.com>
2025-08-04 10:46:35 -0700

    Opened port 443 for jenkins tls certs

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -59,6 +59,15 @@ resource "aws_security_group" "private_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow HTTPS for Lets Encrypt and browser"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   # Allow ICMP (ping) from self
   ingress {
     description = "Allow ping (ICMP) from self"
